50535a5e10b6b17b9557dfb82f8f4f665cbea17f
abderahman elasri <el.abderrahman00@gmail.com>
2020-06-04 17:23:17 +0100

    create tcp socket

--- server.py
+++ server.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+import socket
+
+HOST = '0.0.0.0'
+PORT = 3000
+
+""" Open a TCP server socket """
+def create_socket() :
+    global s 
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(5)
+
+    print(f'Server launched :: {HOST}:{PORT}')
+
+create_socket()
